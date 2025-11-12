78a609939a0996435ae2c075065167af238c4144
Viky <vikyy905@gmail.com>
2025-11-12 18:18:34 +0100

    class PasswordService

--- auth_generations.py
+++ auth_generations.py
@@ -1,6 +1,6 @@
 from abc import abstractmethod, ABC
 
-import Bcrypt
+import bcrypt
 
 class PasswordServiceAbstract(ABC):
     @abstractmethod
@@ -11,3 +11,26 @@ class PasswordServiceAbstract(ABC):
     def verify_password(self, entered_password: str, hashed_password: str) -> bool:
         """Method that verifies password entered by user"""
 
+class PasswordService(PasswordServiceAbstract):
+
+    def hash_password(self, password: str) -> str:
+        # password = bcrypt.hash_password(password)
+        pass_bytes = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hash_password = bcrypt.hashpw(pass_bytes, salt)
+        return hash_password.decode('utf-8')
+
+    def verify_password(self, entered_password: str, hashed_password: str) -> bool:
+        entered_password_bytes = entered_password.encode('utf-8')
+        hashed_password_bytes = hashed_password.encode('utf-8')
+
+        try:
+            return bcrypt.checkpw(entered_password_bytes, hashed_password_bytes)
+        except ValueError:
+            return False
+
+        """
+        entered_password = entered_password.encode('utf-8')
+        res = bcrypt.checkpw(Bytes, hashed_password)
+        return res
+        """
