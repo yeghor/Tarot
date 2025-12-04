81b4e41906a62cdb094be260de7325c4ee70fbbb
RomanovYehor <yeghor.romanov@gmail.com>
2025-12-04 21:37:47 +0200

    connected tailwindcss to frontend app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <p>Hello World!</p>
+    <p className="text-blue-700">Hello World!</p>
   </StrictMode>,
 )
---
+++ frontend/src/mainPage.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+
+const MainPage = () => {
+    
+
+};
+
+export default MainPage;
